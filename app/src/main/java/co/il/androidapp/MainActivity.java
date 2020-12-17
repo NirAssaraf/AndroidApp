83448ad09b6d91d4476ed343cd56ef9ca7f86e31
NirAssaraf <assarafnir@gmail.com>
2020-12-17 22:21:36 +0200

    add main and singup fragments

--- app/src/main/java/co/il/androidapp/MainActivity.java
+++ app/src/main/java/co/il/androidapp/MainActivity.java
@@ -1,8 +1,12 @@
 package co.il.androidapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,4 +15,15 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+    public static class MainFragment extends Fragment {
+        @Override
+        public View onCreateView(LayoutInflater inflater,
+                                 ViewGroup container,
+                                 Bundle savedInstanceState) {
+            // Inflate the layout for this fragment
+            return inflater.inflate(R.layout.main_fragment,
+                    container, false);
+        }
+    }
 }
+
